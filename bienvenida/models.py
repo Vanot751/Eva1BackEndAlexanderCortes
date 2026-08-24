c28c5b5f4a5af40054fbb80032e066a60dcd4b7b
Alexander Cortés <167124531+Vanot751@users.noreply.github.com>
2026-08-24 13:28:48 -0400

    Update 24-08

--- bienvenida/models.py
+++ bienvenida/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models 
+class Producto(models.Model): 
+    nombre = models.CharField(max_length=64) 
+    precio = models.IntegerField(default=0) 
+    descripcion = models.CharField(max_length=500, null=True, blank=True)
+    stock =  models.IntegerField(default=0)
