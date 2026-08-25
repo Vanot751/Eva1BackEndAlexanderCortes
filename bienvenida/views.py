001ae189692c05d7711e57a31ad09a98676d326d
Alexander Cortés <167124531+Vanot751@users.noreply.github.com>
2026-08-25 13:30:05 -0400

    Update 25-08 BackEnd

--- bienvenida/views.py
+++ bienvenida/views.py
@@ -4,10 +4,9 @@ from .models import Producto
 
 # Create your views here.
 def inicio(request): 
-
     return HttpResponse("Hola mundo desde Django")
-
 
 def lista_productos(request):
     productos = Producto.objects.all()
     return render(request, "productos/lista.html", {"productos": productos})
+
